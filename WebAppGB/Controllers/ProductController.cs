ac0481dc9d1419f599c8d3c1d569e1830cb2468e
Neddy <Ned@gmail.com>
2024-07-08 21:13:44 +0300

    Добавил контроллер

---
+++ WebAppGB/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppGB.Data;
+using WebAppGB.Models;
+
+namespace WebAppGB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> AddProduct(string name, string description, decimal price) 
+        {
+            using(Context context = new Context())
+            {
+                if (context.Products.Any(p => p.ProductName == name)) 
+                {
+                    return StatusCode(409);
+                }
+                var product = new Product { ProductName = name, Description = description, Price = price };
+                context.Products.Add(product);
+                context.SaveChanges();
+                return Ok(product.ID);
+            }
+        }
+        [HttpGet]
+        public ActionResult GetAllProducts()
+        {
+            using(Context context = new Context()) 
+            { 
+                var products = context.Products.Select(p => new Product { ProductName = p.ProductName, Description = p.Description, Price = p.Price}).ToList();
+                return Ok(products);
+            }
+        }
+    }
+}
--- WebAppGB/Models/Product.cs
+++ WebAppGB/Models/Product.cs
@@ -5,7 +5,7 @@
         public int ID { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public int? ProductGroupID { get; set; }
         public virtual List<Storage> Storages { get; set; }
         public virtual ProductGroup? ProductGroup { get; set; }
