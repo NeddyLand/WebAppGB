ed29c4034057447d832aa74b2c3fe388bad9c26a
Neddy <Ned@gmail.com>
2024-07-08 20:19:34 +0300

    Добавил миграцию

--- WebAppGB/Data/Context.cs
+++ WebAppGB/Data/Context.cs
@@ -1,8 +1,50 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebAppGB.Models;
 
 namespace WebAppGB.Data
 {
     public class Context : DbContext
     {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductGroup> ProductGroups { get; set; }
+        public virtual DbSet<Storage> Storages { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Data Source = .\\SQLEXPRESS; Initial catalog = Store; Trusted_Connection = True; TrustServerCertificate = True").UseLazyLoadingProxies().LogTo(Console.WriteLine);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductGroup>(entity =>
+            {
+                entity.HasKey(pg => pg.ID)
+                      .HasName("product_group_pk");
+
+                entity.ToTable("ProductGroups");
+
+                entity.Property(pg => pg.ID)
+                      .HasColumnName("pg_name")
+                      .HasMaxLength(255);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.ID)
+                      .HasName("product_pk");
+
+                entity.ToTable("Products");
+
+                entity.Property(p => p.ID)
+                      .HasColumnName("prd_name")
+                      .HasMaxLength(255);
+
+                entity.HasOne(p => p.ProductGroup).WithMany(pg => pg.Products).HasForeignKey(p => p.ProductGroupID);
+            });
+            modelBuilder.Entity<Storage>(entity =>
+            {
+                entity.HasKey(p => p.ID)
+                      .HasName("storage_pk");
+
+                entity.ToTable("Storages");
+
+                entity.HasMany(s => s.Products).WithMany(p => p.Storages);
+            });
+        }
     }
 }
---
+++ WebAppGB/Migrations/20240708171209_InitialCreate.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppGB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductGroups",
+                columns: table => new
+                {
+                    pg_name = table.Column<int>(type: "int", maxLength: 255, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_group_pk", x => x.pg_name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("storage_pk", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    prd_name = table.Column<int>(type: "int", maxLength: 255, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductGroupID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_pk", x => x.prd_name);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductGroups_ProductGroupID",
+                        column: x => x.ProductGroupID,
+                        principalTable: "ProductGroups",
+                        principalColumn: "pg_name");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductStorage",
+                columns: table => new
+                {
+                    ProductsID = table.Column<int>(type: "int", nullable: false),
+                    StoragesID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductStorage", x => new { x.ProductsID, x.StoragesID });
+                    table.ForeignKey(
+                        name: "FK_ProductStorage_Products_ProductsID",
+                        column: x => x.ProductsID,
+                        principalTable: "Products",
+                        principalColumn: "prd_name",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductStorage_Storages_StoragesID",
+                        column: x => x.StoragesID,
+                        principalTable: "Storages",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductGroupID",
+                table: "Products",
+                column: "ProductGroupID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductStorage_StoragesID",
+                table: "ProductStorage",
+                column: "StoragesID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductStorage");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropTable(
+                name: "ProductGroups");
+        }
+    }
+}
